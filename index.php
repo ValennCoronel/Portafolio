5cb2d3e5420689b39608aa704d959d91af81936d
valen <coronel.valentino@hotmail.com>
2021-12-22 01:45:27 -0300

    cambio css

--- index.php
+++ index.php
@@ -1,12 +1,110 @@
 <!DOCTYPE html>
 <html lang="es">
+  
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <link rel="stylesheet" href="css/styles.css">
   <title>Document</title>
 </head>
+
 <body>
-  <nav></nav>
+
+  <!--  HEADER  -->
+
+  <header>
+    <nav>
+      <a href="#"> <img src="" alt="" id="logo"> logo </a>
+      <ul>
+        <li> <a href="#"> Home </a> </li>
+        <li> <a href="#"> ¿Quién soy? </a> </li>
+        <li> <a href="#"> Portfolio </a> </li>
+        <li> <a href="#"> Tecnologias </a> </li>
+        <li> <a href="#"> Contacto </a> </li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- HOME -->
+  <div class="container" id="home">
+    <div id="txtHome"></div>
+    <div id="imgHome"></div>
+  </div>
+
+  <!-- QUIEN SOY -->
+  <div class="container" id="quienSoy">
+    <div id="txtQuienSoy"></div>
+
+  </div>
+
+
+  <!-- PORTFOLIO -->
+
+  <div class="container" id="portfolio">
+    <div id="txtPortfolio"></div>
+    <div id="containerPortfolio">
+      <div class="proyect"></div>
+      <div class="proyect"></div>
+      <div class="proyect"></div>
+      <div class="proyect"></div>
+      <div class="proyect"></div>
+      <div class="proyect"></div>
+    </div>
+  </div>
+
+  <!-- TECNOLOGIAS -->
+  <div class="container" id="tecnologias">
+    <div id="txtTecnologias"></div>
+    <div id="containerTecnologias">
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+      <div class="languajes"></div>
+    </div>
+  </div>
+
+
+  <!-- CONTACTO -->
+  <div class="container" id="contacto">
+    <div id="txtContacto"></div>
+    <div id="secContacto">
+      <form action="">
+        <input type="text" name="" id="">
+        <input type="email" name="" id="">
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <input type="submit" value="enviar">
+      </form>
+    </div>
+    <div class="secContacto">
+      <div id="datosContacto"></div>
+      <div id="redes">
+        <div class="iconRedes"></div>
+        <div class="iconRedes"></div>
+        <div class="iconRedes"></div>
+        <div class="iconRedes"></div>
+      </div>
+    </div>
+  </div>
+
+  <!-- FOOTER -->
+  <div class="container" id="footer">
+    <div id="legal"></div>
+    <div id="closeFooter">
+      <div class="iconRedesFooter"></div>
+      <div class="iconRedesFooter"></div>
+      <div id="emailFooter"></div>
+      <div class="iconRedesFooter"></div>
+      <div class="iconRedesFooter"></div>
+    </div>
+  </div>
+
 </body>
+
 </html>
